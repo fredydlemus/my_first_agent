3eba5e6c6f2424225f3180ad32f480396152ce66
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-11-11 13:51:43 -0600

    add messages to agent state

--- src/agents/simple.py
+++ src/agents/simple.py
@@ -1,18 +1,22 @@
-from typing import TypedDict
 from langgraph.graph import StateGraph, START, END
+from langgraph.graph import MessagesState
+from langchain_core.messages import AIMessage
 
-class State(TypedDict):
+class State(MessagesState):
     customer_name: str
     my_age: int
 
 def node_1(state: State):
+    history = state["messages"]
     if state.get("customer_name") is None:
         return {
             "customer_name": "Alice"
         }
-    return {
-        "my_age": 30
-    }
+    else:
+        ai_message = AIMessage(content="Hello, how can I assist you today?")
+        return {
+            "messages": [ai_message]
+        }
 
 builder = StateGraph(State)
 builder.add_node("node_1", node_1)
