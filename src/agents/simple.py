85ef170553c2ff76eba0fc17eec1cacf2c4d3202
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-11-11 11:31:22 -0600

    add simple state graph

---
+++ src/agents/simple.py
@@ -0,0 +1,23 @@
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+class State(TypedDict):
+    customer_name: str
+    my_age: int
+
+def node_1(state: State):
+    if state.get("customer_name") is None:
+        return {
+            "customer_name": "Alice"
+        }
+    return {
+        "my_age": 30
+    }
+
+builder = StateGraph(State)
+builder.add_node("node_1", node_1)
+
+builder.add_edge(START, "node_1")
+builder.add_edge("node_1", END)
+
+agent = builder.compile()
