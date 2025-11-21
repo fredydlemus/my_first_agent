3a99883bac6a01e11b0c0599c201cf5057fc3239
Fredy Flores Lemus <fredy@Fredys-MacBook-Pro.local>
2025-11-21 12:45:52 -0600

    prompt chain step two

--- src/agents/rag.py
+++ src/agents/rag.py
@@ -1,8 +1,7 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph import MessagesState
-from langchain_core.messages import AIMessage
 from langchain.chat_models import init_chat_model
-import random
+from pydantic import BaseModel, Field
 
 llm = init_chat_model("openai:gpt-4o", temperature=1)
 file_search_tool = {
@@ -13,23 +12,38 @@ llm = llm.bind_tools([file_search_tool])
 
 class State(MessagesState):
     customer_name: str
-    my_age: int
+    phone: str
+    my_age: str
+
+class ContactInfo(BaseModel):
+    """Contact information for a person."""
+    name: str = Field(description="The name of the person")
+    emial: str = Field(description="The email adress of the person")
+    phone: str = Field(description="the phone number of the person")
+    age: str = Field(description="the age of the person")
+
+llm_with_structured_output = init_chat_model("claude-sonnet-4-20250514", temperature=0)
+llm_with_structured_output = llm_with_structured_output.with_structured_output(schema=ContactInfo)
 
 def extractor(state: State):
-    return {}
+    history = state["messages"]
+    customer_name = state.get("customer_name", None)
+    new_state: State = {}
+    if customer_name is None or len(history) > 10:
+        schema = llm_with_structured_output.invoke(history)
+        new_state["customer_name"] = schema.name
+        new_state["phone"] = schema.phone
+        new_state["my_age"] = schema.age
+    return new_state
 
 def conversation(state: State):
     new_state: State = {}
-    if state.get("customer_name") is None:
-        new_state["customer_name"] = "Alice"
-    else:
-        new_state["my_age"] = random.randint(20, 50)
-
     history = state["messages"]
     last_message = history[-1]
-    ai_message = llm.invoke(last_message.text)
+    customer_name = state.get("customer_name", 'John')
+    system_message = f"You are a helpful assistant that can answer questions about the customer {customer_name}"
+    ai_message = llm.invoke([("system", system_message), ("user", last_message.text)])
     new_state["messages"] = [ai_message]
-    print(new_state)
     return new_state
 
 builder = StateGraph(State)
